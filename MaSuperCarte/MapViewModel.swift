c228d86400ecf891e20ac22d4998588a576353d2
Guillaume Bellut <guillaume@bellut.com>
2017-05-01 12:02:07 +0200

    Added MapViewModel.

---
+++ MaSuperCarte/MapViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  MapViewModel.swift
+//  MaSuperCarte
+//
+//  Created by Guillaume Bellut on 01/05/2017.
+//  Copyright © 2017 Guillaume Bellut. All rights reserved.
+//
+
+import Foundation
